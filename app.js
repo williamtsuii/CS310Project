52a4cc723e4b2f80461e739859a2b34e60cdaf19
Alex Tsang <atsang1117@hotmail.com>
2016-01-22 15:30:57 -0800

    added bootstrap and grunt to projcet

--- app.js
+++ app.js
@@ -4,7 +4,9 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-var firebase = require('firebase');
+var Firebase = require('firebase');
+var rootRef = new Firebase('https://cs310.firebaseio.com/data');
+var userCollection = rootRef.child('users');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
