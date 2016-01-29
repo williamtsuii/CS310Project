7c87a1ab64d8cf6a8b2104f3a04eb46471d4f72b
Alex Tsang <atsang1117@hotmail.com>
2016-01-29 15:04:45 -0800

    added firebase integration for user creation

--- routes/index.ts
+++ routes/index.ts
@@ -1,19 +1,20 @@
 ///<reference path='../types/DefinitelyTyped/node/node.d.ts'/>
 ///<reference path="../types/DefinitelyTyped/express/express.d.ts"/>
-var Router = (function () {
-    function Router() {
-    }
-    Router.prototype.constructo = function () {
-    };
-    Router.prototype.start = function () {
+
+class Router {
+    constructo() {
+    } start() {
+
         var express = require('express');
         var router = express.Router();
         var Firebase = require('firebase');
         var refRoot = new Firebase('https://cs310.firebase.com');
+
         /* GET home page. */
-        router.get('/', function (req, res, next) {
+        router.get('/', function(req, res, next) {
             res.render('index', { title: 'Express' });
         });
+        
         /*PUT users to userdb */
         router.get('/adduser', function (req, res) {
             //users are saved seperately, no need to go to database. firebase automatically adds it to their own database for only users.
@@ -21,41 +22,59 @@ var Router = (function () {
             //cuz they ask us for object fucking orineted code. this just makes it slower
             var user = new User(req.body.email, req.body.password);
             var userID;
+            
             //create new user using firebase api
             users.createUser({
                 email: user.getEmail(),
                 password: user.getPassword()
-            }, function (error, userData) {
+            }, function(error, userData) {
                 if (error) {
                     console.log("Error creating User");
-                }
-                else {
+                } else {
                     userID = userData.id;
                     console.log("Succesful creating of user with it", userData.id);
                 }
             });
+            
             //in here, access data in firebase, make a node with email as the key and add other details needed so we can connect to the user: 
             //example: preference list, editor?, name, date of birth, etc
+       
             var uUserDB = refRoot.child('users/' + userID);
-            uUserDB.set({});
+            uUserDB.set({
+                //data inside
+            });
         });
+
         module.exports = router;
-    };
-    return Router;
-})();
-var User = (function () {
-    function User(email, password) {
+    }
+}
+
+
+interface User {
+    
+    getEmail(): string;
+    getPassword(): string;
+    
+}
+
+class User implements User {
+    
+    private email: string;
+    private password: string;
+    
+    constructor(email : string, password : string) {
         this.email = email;
         this.password = password;
     }
-    User.prototype.getEmail = function () {
-        return this.email;
+    
+    getEmail() {
+      return this.email;  
     };
-    ;
-    User.prototype.getPassword = function () {
+    
+    getPassword() {
         return this.password;
-    };
-    return User;
-})();
+    } 
+}
+
 var router = new Router();
 router.start();
