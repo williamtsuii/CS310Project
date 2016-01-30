c37e2d0e8f116811c401e8639d16cf3f28afa5d0
Alex Tsang <atsang1117@hotmail.com>
2016-01-29 16:30:41 -0800

    changed app.js to be ts and converted it back using compiler

--- app.ts
+++ app.ts
@@ -1,9 +1,13 @@
 ///<reference path='../project/types/DefinitelyTyped/node/node.d.ts'/>
 ///<reference path="../project/types/DefinitelyTyped/express/express.d.ts"/>
-var Application = (function () {
-    function Application() {
-    }
-    Application.prototype.start = function () {
+interface Error{
+    status? : number;
+}
+
+class Application {
+
+    constructor() {
+    } start() {
         var express = require('express');
         var path = require('path');
         var favicon = require('serve-favicon');
@@ -13,12 +17,16 @@ var Application = (function () {
         var Firebase = require('firebase');
         var rootRef = new Firebase('https://cs310.firebaseio.com/data');
         var userCollection = rootRef.child('users');
+
         var routes = require('./routes/index');
         var users = require('./routes/users');
+
         var app = express();
+
         // view engine setup
         app.set('views', path.join(__dirname, 'views'));
         app.set('view engine', 'jade');
+
         // uncomment after placing your favicon in /public
         //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
         app.use(logger('dev'));
@@ -26,19 +34,23 @@ var Application = (function () {
         app.use(bodyParser.urlencoded({ extended: false }));
         app.use(cookieParser());
         app.use(express.static(path.join(__dirname, 'public')));
+
         app.use('/', routes);
         app.use('/users', users);
+
         // catch 404 and forward to error handler
-        app.use(function (req, res, next) {
+        app.use(function(req, res, next) {
             var err = new Error('Not Found');
             err.status = 404;
             next(err);
         });
+
         // error handlers
+
         // development error handler
         // will print stacktrace
         if (app.get('env') === 'development') {
-            app.use(function (err, req, res, next) {
+            app.use(function(err, req, res, next) {
                 res.status(err.status || 500);
                 res.render('error', {
                     message: err.message,
@@ -46,18 +58,20 @@ var Application = (function () {
                 });
             });
         }
+
         // production error handler
         // no stacktraces leaked to user
-        app.use(function (err, req, res, next) {
+        app.use(function(err, req, res, next) {
             res.status(err.status || 500);
             res.render('error', {
                 message: err.message,
                 error: {}
             });
         });
+
+
         module.exports = app;
-    };
-    return Application;
-})();
+    }
+}
 var application = new Application();
 application.start();
