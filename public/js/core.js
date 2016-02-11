6d0639bf2e9cf262a8c4144b0a80bf09684d1eb0
banafshehderayat <b.derayat@gmail.com>
2016-02-10 21:38:27 -0800

    added profile

--- public/js/core.js
+++ public/js/core.js
@@ -2,41 +2,38 @@ angular.module('scotchTodo', ['userService','pageService','ngRoute'])
 
     .config(['$routeProvider', function($routeProvider) {
         $routeProvider
-            .when('/signup', {templateUrl: 'signup.html',   controller: signupController})
-            .when('/home', {templateUrl:'home.html', controller:homeController})
+            .when('/signup', {templateUrl: 'signup.html',   controller: 'signupController as signUp'})
+            .when('/home', {templateUrl:'home.html', controller:'homeController as home'})
 
-            .when('/profile', {templateUrl:'profile.html', controller:profileController})
+            .when('/profile', {templateUrl:'profile.html', controller:'profileController as profile'})
 
-            .when('/create', {templateUrl:'create.html', controller:createController}) 
+            .when('/create', {templateUrl:'create.html', controller:'createController as create'})
             .otherwise({redirectTo: '/home'});
     }]);
-
 var id;
 var u;
 
 function MainCtrl($scope, Page, User) {
+    var main = this;
     console.log('MainCtrl loaded!');
     $scope.Page = Page;
     $scope.User = User;
+
 }
 
 
 function signupController($scope, User, Page) {
+    var signUp = this;
     Page.setTitle('Sign Up');
     $scope.formData = {};
     $scope.signupUser = function() {
         // validate the formData to make sure that something is there
         // if form is empty, nothing will happen
-        if ($scope.formData.email != undefined) {
-            $scope.loading = true;
-
-            // call the create function from our service (returns a promise object)
-            User.signup($scope.formData)
+        if (signUp.formData.email != undefined) {
+            User.signup(signUp.formData)
                 .success(function(data) {
                     id = data;
                     window.location.replace('/#/profile');
-                    //console.log(data);
-                    //$scope.formData = {}; // clear the form so our user is ready to enter another
 
                 });
         }
@@ -45,18 +42,21 @@ function signupController($scope, User, Page) {
 
 
 function profileController($scope, User, Page){
+    var profile = this;
     console.log(id);
     Page.setTitle('Profile');
     User.view(id)
         .success(function(data){
-            console.log(data);
+            profile.user = data;
+            console.log(profile.user.preferences);
         });
+    $scope.createComic = function(){
+        window.location.replace('/#/create');
+    }
 }
 
 function createController($scope, Comic, Page) {
     Page.setTitle("New Comic");
-    console.log(Page.title());
-    $scope.formData = {};
     $scope.createComic = function() {
         console.log($scope.formData);
         // validate the formData to make sure that something is there
@@ -72,19 +72,17 @@ function createController($scope, Comic, Page) {
 
 
 function homeController($scope, User, Page){
+    var home = this;
     Page.setTitle('Home');
-    $scope.user = {};
     $scope.loginUser = function(){
-        console.log($scope.user.email);
-        User.login($scope.user)
+        console.log(home.user.email);
+        User.login(home.user)
             .success(function (data) {
-                u = $scope.user;
+                u = home.user;
                 console.log(u);
                 id = data;
-                $scope.user = {};
-
+                home.user = {};
                 window.location.replace('/#/profile');
-
             });
     }
 
